00acda2adb5b91a0ca0d6ce8a638f856a9eaf9dd
xeniys <135637379+xeniys@users.noreply.github.com>
2025-07-21 15:10:26 +0200

    Введение в Pytest

---
+++ tests/test_pytest.py
@@ -0,0 +1,10 @@
+def test_user_login():
+    print("Hello!")
+
+
+def test_assert_positive_case():
+    assert (2 + 2) == 4
+
+
+def test_assert_negative_case():
+    assert (2 + 2) == 5, "(2 + 2) != 5"
