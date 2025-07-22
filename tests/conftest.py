be56f7a5498c5aafe8a1a54862ab09e1a275359a
xeniys <135637379+xeniys@users.noreply.github.com>
2025-07-22 21:50:56 +0200

    Фикстуры Pytest. Практика работы с pytest фикстурами

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,3 +8,35 @@ def chromium_page(playwright) -> Page:
     page = browser.new_page()
 
     yield page
+
+
+@pytest.fixture(scope="session")
+def initialize_browser_state(playwright):
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
+
+    email = page.get_by_test_id('registration-form-email-input').locator('input')
+    email.fill('user.name@gmail.com')
+
+    username = page.get_by_test_id('registration-form-username-input').locator('input')
+    username.fill('username')
+
+    password = page.get_by_test_id('registration-form-password-input').locator('input')
+    password.fill('password')
+
+    reg_button = page.get_by_test_id('registration-page-registration-button')
+    reg_button.click()
+
+    context.storage_state(path='browser-state.json')
+
+
+@pytest.fixture
+def chromium_page_with_state(initialize_browser_state, playwright: Playwright) -> Page:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context(storage_state='browser-state.json')
+    page = context.new_page()
+
+    yield page
+
--- tests/test_courses.py
+++ tests/test_courses.py
@@ -1,49 +1,25 @@
-from playwright.sync_api import sync_playwright, expect
+from playwright.sync_api import sync_playwright, expect, Playwright
 import pytest
 
 
 @pytest.mark.courses
 @pytest.mark.regression
-def test_empty_courses_list():
-    with sync_playwright() as playwright:
-        browser = playwright.chromium.launch(headless=False)
-        context = browser.new_context()
-        page = context.new_page()
+def test_empty_courses_list(chromium_page_with_state, playwright):
+    page = chromium_page_with_state
 
-        page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
+    page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses')
 
-        email = page.get_by_test_id('registration-form-email-input').locator('input')
-        email.fill('user.name@gmail.com')
+    header_courses = page.get_by_test_id('courses-list-toolbar-title-text')
+    expect(header_courses).to_be_visible()
+    expect(header_courses).to_have_text('Courses')
 
-        username = page.get_by_test_id('registration-form-username-input').locator('input')
-        username.fill('username')
+    icon = page.get_by_test_id('courses-list-empty-view-icon')
+    expect(icon).to_be_visible()
 
-        password = page.get_by_test_id('registration-form-password-input').locator('input')
-        password.fill('password')
+    courses_info = page.get_by_test_id('courses-list-empty-view-title-text')
+    expect(courses_info).to_be_visible()
+    expect(courses_info).to_have_text('There is no results')
 
-        reg_button = page.get_by_test_id('registration-page-registration-button')
-        reg_button.click()
-
-        context.storage_state(path='browser-state-courses.json')
-
-    with sync_playwright() as playwright:
-        browser = playwright.chromium.launch(headless=False)
-        context = browser.new_context(storage_state='browser-state.json')
-        page = context.new_page()
-
-        page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses')
-
-        header_courses = page.get_by_test_id('courses-list-toolbar-title-text')
-        expect(header_courses).to_be_visible()
-        expect(header_courses).to_have_text('Courses')
-
-        icon = page.get_by_test_id('courses-list-empty-view-icon')
-        expect(icon).to_be_visible()
-
-        courses_info = page.get_by_test_id('courses-list-empty-view-title-text')
-        expect(courses_info).to_be_visible()
-        expect(courses_info).to_have_text('There is no results')
-
-        description = page.get_by_test_id('courses-list-empty-view-description-text')
-        expect(description).to_be_visible()
-        expect(description).to_have_text('Results from the load test pipeline will be displayed here')
+    description = page.get_by_test_id('courses-list-empty-view-description-text')
+    expect(description).to_be_visible()
+    expect(description).to_have_text('Results from the load test pipeline will be displayed here')
